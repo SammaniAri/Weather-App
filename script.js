fe3dc565cd81961c8f20556d7a663817e9a582ac
Sammani Ariyarathne <sammani.ari@gmail.com>
2022-11-03 22:37:01 +0100

    celsius

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 "use strict";
 async function getData() {
-    const res = await fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=04c8beabeded4281662ceda5a1cce41c");
+    const res = await fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=04c8beabeded4281662ceda5a1cce41c&units=metric");
     const data = await res.json();
   
     const heading =document.getElementById("heading");
   console.log(data);
     let cityName= (data.name);
   let cityTemperature=(data.main.temp)
-  heading.innerHTML ="Currently "+ cityTemperature + " in " + cityName;
+  heading.innerHTML ="Currently "+ cityTemperature + " ℃ in " + cityName;
+
+  let lowestTemperature= (data.main.temp_min);
+  let highestTemperature=(data.main.temp_max);
+
+  subheading2.innerHTML =lowestTemperature + " ℃ / " +highestTemperature + "℃";
    
     //document.body.append(img);
   }
