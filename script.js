875df97af3a9591b6058cdb67d1ccecc85a7be65
Sammani Ariyarathne <sammani.ari@gmail.com>
2022-11-04 13:54:07 +0100

    https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 async function getData(city) {
-    const res = await fetch("http://api.openweathermap.org/data/2.5/weather?q="+ city + "&APPID=04c8beabeded4281662ceda5a1cce41c&units=metric");
+    const res = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city + "&APPID=04c8beabeded4281662ceda5a1cce41c&units=metric");
     const data = await res.json();
   
     const heading =document.getElementById("heading");
